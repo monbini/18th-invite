6bf35e349a81fe48d099fe345bde57d404a8d288
monbini <argamariaconcepcion@gmail.com>
2025-05-01 22:10:27 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function handleDetailsClick() {
+    window.location.href = "details.html";
+}
+
+function handleThemeClick() {
+    window.location.href = "theme.html"
+    }
+
+function handleReturnClick() {
+    window.location.href = "index.html";
+}
